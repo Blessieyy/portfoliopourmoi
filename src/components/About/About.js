4b00c1d11449ebcdb55d4a1b9e2c124914fcb396
Blessiey <mogashoablessing676@gmail.com>
2025-02-06 16:00:32 +0200

    Commit

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -22,15 +22,21 @@ function About() {
             <div className="aboutItemText">
               <h3 className="aboutHeader">Frontend Developer</h3>
               <p className="aboutText">
-                I'm a frontend developer with experience in building responsive
-                and optimised websites
+                I'm a frontend developer passionate about building responsive,
+                high-performance websites with seamless user experiences. I
+                specialize in modern web technologies like React, ensuring
+                clean, optimized, and accessible designs. From crafting
+                intuitive UIs to optimizing performance, I focus on creating
+                fast, engaging, and user-friendly web applications. I love
+                solving challenges and bringing ideas to life with efficient,
+                maintainable code.
               </p>
             </div>
           </li>
           <li className="aboutItem">
             <img src={Server} alt="ServerIcon" />
             <div className="aboutItemText">
-              <h3  className="aboutHeader">Backend Developer</h3>
+              <h3 className="aboutHeader">Backend Developer</h3>
               <p className="aboutText">
                 I have experience in developing fast and optimised Backend
                 systems and APIs
@@ -40,7 +46,7 @@ function About() {
           <li className="aboutItem">
             <img src={UI} alt="UIIcon" />
             <div className="aboutItemText">
-              <h3  className="aboutHeader">UI Designer</h3>
+              <h3 className="aboutHeader">UI Designer</h3>
               <p className="aboutText">
                 I have designed multiple landing pages and designed systems as
                 well
--- src/components/Project/MobileProjects.js
+++ src/components/Project/MobileProjects.js
@@ -1,9 +1,44 @@
+import "../../components/Project/Project.css";
 import React from 'react'
+import mobile from "../../data/mobile.json";
+
+
+import ProjectCard from "./ProjectCard";
 
 function MobileProjects() {
-  return (
-    <div>MobileProjects</div>
-  )
-}
+ 
+    return (
+      <section className="project-container" id="project">
+        <div className="projects">
+          {mobile.map((project, id) => {
+            return <ProjectCard key={id} project={project} />;
+          })}
+        </div>
+        <h2 className="project-title">Other Projects</h2>
+        {/* <div className="others">
+          {Oprojects.map((others, id) => {
+            return (
+              <ul>
+                <div className="others-content">
+                <li key={id} >
+                 <h3 className="others-title">{others.title2}</h3> 
+                 </li>
+                 <p className="desc2">{others.description2}</p>
+             
+                <div className="links2">
+                <a href=  {others.demo2} className="link2">Demo</a>
+                <a href= {others.source2} className="link2">Source</a>
+                </div>
+                </div> 
+              </ul>
+              
+            );
+          })}
+        </div> */}
+      </section>
+    );
+  }
+  
+
 
 export default MobileProjects
